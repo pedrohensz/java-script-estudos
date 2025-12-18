1fd8e9fdf195dde94840a64d06d2956d8416f945
pedrohensz <pedro.barbosa2730@gmail.com>
2025-12-18 10:13:19 -0300

    Feat-Pastas das aulas

---
+++ Aula-8-Objeto-Matemático/index.js
@@ -0,0 +1,24 @@
+//Math= Objeto nativa que provê uma coleção de propriedades e métodos
+
+// pi
+Math.PI 
+console.log(Math.PI)
+
+//EULER
+Math.E
+console.log(Math.E)
+
+let x = 3.21;
+let y = 2;
+let k = 3.99;
+let z;
+//arredondar
+z = Math.round(x);
+
+console.log(z);
+
+k = Math.floor(k);
+console.log(k);
+
+k = Math.ceil(k);
+console.log(k);
