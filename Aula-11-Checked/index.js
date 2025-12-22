dced3cfbcb8a39c2aa49323bd12eaf28d210432d
pedrohensz <pedro.barbosa2730@gmail.com>
2025-12-22 06:32:10 -0300

    Feat:Aula 11 realizada.

--- Aula-11-Checked/index.js
+++ Aula-11-Checked/index.js
@@ -1,1 +1,28 @@
 //
+const subscribe = document.getElementById("myCheckbox");
+const mastercard = document.getElementById("mastercardBtn");
+const visa = document.getElementById("visaBtn");
+const paypal = document.getElementById("paypalBtn");
+const submit = document.getElementById("submitBtn");
+const subscibreinfo = document.getElementById("subscribeInfo");
+const paymentinfo = document.getElementById("paymentInfo");
+
+
+submit.onclick = function(){
+    if(subscribe.checked){
+        subscibreinfo.textContent = "Você está inscrito.";
+        
+    }
+    else{
+        subscibreinfo.textContent = "Você não está inscrito."
+    };
+    if(mastercard.checked){
+        paymentinfo.textContent = "Você pagou utilizando Mastercard."
+    }
+    else if (paypal.checked){
+        paymentinfo.textContent = "Você pagou utilizando Paypal."    
+    }
+    else if (visa.checked){
+        paymentinfo.textContent = "Você pagou utilizando Visa."    
+    }
+}
